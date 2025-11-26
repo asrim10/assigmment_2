00a45e2c9e6807dba1ad3fbedb39df3734b894aa
asrim10 <asrimmsuwall@gmail.com>
2025-11-26 09:41:11 +0545

    custom widgets button and text_form_field made

---
+++ lib/screens/custom_layout_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomLayoutScreen extends StatelessWidget {
+  const CustomLayoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
